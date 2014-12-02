d3dba786f1dab90c786313e4f8127e13b3398b61
John Park <John.Park@ecs.vuw.ac.nz>
2014-12-02 19:00:40 +1300

    Demirkol Benchmark will use external dataset instead of internal resource.

--- JSS_Research/src/jss/problem/static_problem/demirkol_dataset/DemirkolDataset.java
+++ JSS_Research/src/jss/problem/static_problem/demirkol_dataset/DemirkolDataset.java
@@ -1,21 +1,20 @@
 package jss.problem.static_problem.demirkol_dataset;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
+import java.util.Map;
 
 import jss.IDataset;
 import jss.IProblemInstance;
 import jss.ProblemSize;
 import jss.problem.static_problem.StaticInstance;
+import jss.problem.static_problem.StaticJob;
+import jss.problem.static_problem.StaticMachine;
 
 /**
  * TODO javadoc.
@@ -25,8 +24,9 @@ import jss.problem.static_problem.StaticInstance;
  */
 public class DemirkolDataset implements IDataset {
 
-	private static final String DEMIRKOL_DATASET = "DemirkolBenchmarksJobShop";
+	private static final String DEMIRKOL_DATASET = "dataset/DemirkolBenchmarksJobShop/";
 	private static final String DEMIRKOL_FILE_SUFFIX = ".txt";
+	private static final String DEMIRKOL_BOUNDS_FILE = "dmu_bounds.csv";
 
 	private static final int TRAINING_SMALL_MACHINE_NUM = 15;
 	private static final int TRAINING_SMALL_JOB_NUM = 15;
@@ -35,6 +35,7 @@ public class DemirkolDataset implements IDataset {
 	private static final int TRAINING_LARGE_MACHINE_NUM = 20;
 	private static final int TRAINING_LARGE_JOB_NUM = 100;
 
+	private Map<String, StaticInstance> problemNameMap = new HashMap<String, StaticInstance>();
 	private List<StaticInstance> problemInstances = new ArrayList<StaticInstance>();
 
 	private List<IProblemInstance> smallInstances = new ArrayList<IProblemInstance>();
@@ -46,80 +47,98 @@ public class DemirkolDataset implements IDataset {
 	 */
 	public DemirkolDataset() {
 		// Read the .txt files.
-		readFile();
+		readDirectory();
 
 		// Generate the lists of the training sets.
 		generateTrainingSets();
-	}
-
-	private void readFile() {
-		List<InputStream> inputStreams = (isRunningFromJar()) ? getStreamsFromJar() : getStreamsFromDir();
 		
-		readProblemInstances(inputStreams);
+		// Read in the upper and lower bounds.
+		readBounds();
 	}
 
-	private boolean isRunningFromJar() {
-		URL datasetURL = DemirkolDataset.class.getResource(DEMIRKOL_DATASET);
-
-		return datasetURL.toString().startsWith("jar:");
-	}
-
-	// TODO fix up the exception handling sometime later.
-	private List<InputStream> getStreamsFromDir() {
-		try {
-			List<InputStream> inputStreams = new ArrayList<InputStream>();
-	
-			File dir = new File(DemirkolDataset.class.getResource(DEMIRKOL_DATASET).toURI());
-			for (File file : dir.listFiles()) {
-				if (file.getName().endsWith(DEMIRKOL_FILE_SUFFIX)) {
-					inputStreams.add(new FileInputStream(file));
-				}
+	private void readDirectory() {
+		File datasetDir = new File(DEMIRKOL_DATASET);
+		
+		if (!datasetDir.exists() || !datasetDir.isDirectory()) {
+			throw new RuntimeException("Demirkol dataset does not exist at directory \'dataset/DemirkolBenchmarksJobShop\'");
+		}
+		
+		for (File dataFile : datasetDir.listFiles()) {
+			if (!dataFile.getName().endsWith(DEMIRKOL_FILE_SUFFIX)) {
+				continue;
 			}
 			
-			return inputStreams;
-		} catch (URISyntaxException ex) {
-			throw new RuntimeException(ex);
-		} catch (IOException ex) {
-			throw new RuntimeException(ex);
-		}
-	}
-
-	// The fark is this on about? Have a run of this at the university tomorrow to see what 
-	// the heck a jar entry does.
-	private List<InputStream> getStreamsFromJar() {
-		// TODO
-		
-		JarFile jarFile = null;
-		try {
-			String path = new File(DemirkolDataset.class.getResource(DEMIRKOL_DATASET).getPath()).getParent().replaceAll("(!|file:\\\\)", "");
-			System.out.println(path);
-
-			jarFile = new JarFile(path);
-
-			Enumeration<JarEntry> entries = jarFile.entries();
-			while (entries.hasMoreElements()) {
-				JarEntry je = entries.nextElement();
-				if (je.getName().startsWith(".txt")) {
-					System.out.println(je.getName());
-				}
-			}
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		} finally {
 			try {
-				jarFile.close();
-			} catch (Exception e) {
+				FileReader fileReader = new FileReader(dataFile);
+				BufferedReader reader = new BufferedReader(fileReader);
+				
+				String line = reader.readLine();
+				int numJobs = Integer.parseInt(line.split("\\s+")[0]);
+				int numMachines = Integer.parseInt(line.split("\\s+")[1]);
+				
+				StaticInstance problemInstance = new StaticInstance();
+				
+				List<StaticMachine> machines = new ArrayList<StaticMachine>();
+				
+				for (int machine = 0; machine < numMachines; machine++) {
+					machines.add(new StaticMachine(problemInstance));
+				}
+				
+				for (int j = 0; j < numJobs; j++) {
+					line = reader.readLine();
+					
+					String[] split = line.trim().split("\\s+");
+					
+					StaticJob job = new StaticJob();
+					
+					for (int i = 0; i < numMachines; i++) {
+						int machine = Integer.parseInt(split[2 * i]);
+						double processingTime = Double.parseDouble(split[2 * i + 1]);
+						
+						job.offerMachine(machines.get(machine));
+						job.setProcessingTime(machines.get(machine), processingTime);
+					}
+					
+					problemInstance.addJob(job);
+				}
+				
+				problemInstances.add(problemInstance);
+				
+				String problemInstanceName = dataFile.getName().replace(DEMIRKOL_FILE_SUFFIX, "");
+				problemNameMap.put(problemInstanceName, problemInstance);
+				
+				reader.close();
+			} catch (IOException ex) {
+				throw new RuntimeException(ex);
 			}
 		}
-		
-		return null;
 	}
 	
-	private void readProblemInstances(List<InputStream> inputStreams) {
-		// I don't like this idea of having multiple file streams open. Maybe try to find another 
-		// way somehow.
+	private void readBounds() {
+		try {
+			File boundsFile = new File(DEMIRKOL_DATASET + DEMIRKOL_BOUNDS_FILE);
+			FileReader fileReader = new FileReader(boundsFile);
+			BufferedReader reader = new BufferedReader(fileReader);
+			
+			String line = reader.readLine(); // Skip the first line (header).
+			while ((line = reader.readLine()) != null) {
+				String[] split = line.split(",");
+				
+				String problemInstanceName = split[0];
+				double lowerBound = Double.parseDouble(split[3]);
+				double upperBound = Double.parseDouble(split[4]);
+				
+				StaticInstance problemInstance = problemNameMap.get(problemInstanceName);
+				problemInstance.setLowerBound(lowerBound);
+				problemInstance.setUpperBound(upperBound);
+			}
+			
+			reader.close();
+		} catch (IOException ex) {
+			throw new RuntimeException(ex);
+		}
 	}
-
+	
 	private void generateTrainingSets() {
 		for (IProblemInstance problem : problemInstances) {
 			int machineSize = problem.getMachines().size();
@@ -168,11 +187,6 @@ public class DemirkolDataset implements IDataset {
 	@Override
 	public List<IProblemInstance> getTest() {
 		return new ArrayList<IProblemInstance>(problemInstances);
-	}
-
-	// TODO temp code.
-	public static void main(String[] args) {
-		DemirkolDataset dataset = new DemirkolDataset();
 	}
 
 }
