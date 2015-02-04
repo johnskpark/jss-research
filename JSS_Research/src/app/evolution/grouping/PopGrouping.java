d43ce7bd31e4922dc696aed9a25c79a82796d2fe
parkjohn <parkjohn@barretts.ecs.vuw.ac.nz>
2015-02-04 15:36:25 +1300

    Do the work.

---
+++ JSS_Research/src/app/evolution/grouping/PopGrouping.java
@@ -0,0 +1,87 @@
+package app.evolution.grouping;
+
+import ec.EvolutionState;
+import ec.Individual;
+import ec.Subpopulation;
+import ec.gp.GPIndividual;
+import ec.gp.koza.KozaFitness;
+import ec.util.Parameter;
+import app.evolution.IJasimaGrouping;
+
+public class PopGrouping implements IJasimaGrouping {
+
+	private static final long serialVersionUID = 6709369554204377046L;
+
+	private Subpopulation population;
+	private GPIndividual[][] ensemble;
+
+	private GPIndividual[] bestEnsemble = null;
+	private KozaFitness bestEnsembleFitness = new KozaFitness();
+
+	private GPIndividual[] bestEnsembleOfGeneration = null;
+	private KozaFitness bestEnsembleOfGenerationFitness = new KozaFitness();
+
+	@Override
+	public void setup(EvolutionState state, Parameter base) {
+		population = state.population.subpops[0];
+		ensemble = new GPIndividual[1][population.individuals.length];
+
+		bestEnsembleFitness.setStandardizedFitness(state, Double.MAX_VALUE);
+		bestEnsembleOfGenerationFitness.setStandardizedFitness(state, Double.MAX_VALUE);
+	}
+
+	@Override
+	public void groupIndividuals(EvolutionState state, int threadnum) {
+		for (int i = 0; i < population.individuals.length; i++) {
+			ensemble[0][i] = (GPIndividual) population.individuals[i];
+		}
+	}
+
+	@Override
+	public GPIndividual[][] getGroups(Individual ind) {
+		return ensemble;
+	}
+
+	@Override
+	public GPIndividual[] getBestGroup() {
+		return bestEnsemble;
+	}
+
+	@Override
+	public KozaFitness getBestGroupFitness() {
+		return bestEnsembleFitness;
+	}
+
+	@Override
+	public GPIndividual[] getBestGroupForGeneration() {
+		return bestEnsembleOfGeneration;
+	}
+
+	@Override
+	public KozaFitness getBestGroupForGenerationFitness() {
+		return bestEnsembleOfGenerationFitness;
+	}
+
+	@Override
+	public void updateFitness(EvolutionState state, GPIndividual[] indGroup,
+			double fitness) {
+		// Update the best group of generation.
+		if (bestEnsembleOfGenerationFitness.standardizedFitness() > fitness) {
+			bestEnsembleOfGeneration = indGroup;
+			bestEnsembleOfGenerationFitness.setStandardizedFitness(state, fitness);
+		}
+
+		// Update the overall best group.
+		if (bestEnsembleOfGenerationFitness.betterThan(bestEnsembleFitness)) {
+			bestEnsemble = bestEnsembleOfGeneration;
+			bestEnsembleFitness.setStandardizedFitness(state, bestEnsembleOfGenerationFitness.standardizedFitness());
+		}
+	}
+
+	@Override
+	public void clearForGeneration(EvolutionState state) {
+		bestEnsembleOfGeneration = null;
+		bestEnsembleOfGenerationFitness.setStandardizedFitness(state, Double.MAX_VALUE);
+	}
+
+}
