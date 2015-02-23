3820b99a1bb77bea27648697b4c6e8015277ed8d
John Park <John.Park@ecs.vuw.ac.nz>
2015-02-24 12:55:38 +1300

    Do the work.

---
+++ JSS_Research/src/app/evolution/pickardt/JasimaEAProblem.java
@@ -0,0 +1,17 @@
+package app.evolution.pickardt;
+
+import ec.EvolutionState;
+import ec.Individual;
+import ec.Problem;
+import ec.simple.SimpleProblemForm;
+
+public class JasimaEAProblem extends Problem implements SimpleProblemForm {
+
+	@Override
+	public void evaluate(EvolutionState state, Individual ind,
+			int subpopulation, int threadnum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
