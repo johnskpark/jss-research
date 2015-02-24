1b0c8871cf7c29a0a030707e5cf981ef784ee132
parkjohn <parkjohn@barretts.ecs.vuw.ac.nz>
2015-02-24 18:04:08 +1300

    Do the work.

--- JSS_Research/src/app/evolution/pickardt/presetRules/PRFCFS.java
+++ JSS_Research/src/app/evolution/pickardt/presetRules/PRFCFS.java
@@ -2,13 +2,19 @@ package app.evolution.pickardt.presetRules;
 
 import jasima.shopSim.core.PR;
 import jasima.shopSim.core.PrioRuleTarget;
+import jasima.shopSim.prioRules.basic.FCFS;
 
 public class PRFCFS extends PR {
 
+	private static final long serialVersionUID = 1887717590022139151L;
+
+	public PRFCFS() {
+		setTieBreaker(new FCFS());
+	}
+
 	@Override
 	public double calcPrio(PrioRuleTarget entry) {
-		// TODO Auto-generated method stub
-		return 0;
+		return entry.getWeight();
 	}
 
 }
