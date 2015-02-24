32974137e3b584c0d2ef33c67efcc640b80740fe
parkjohn <parkjohn@barretts.ecs.vuw.ac.nz>
2015-02-24 18:00:17 +1300

    Do the work.

---
+++ JSS_Research/src/app/evolution/pickardt/presetRules/PRCR.java
@@ -0,0 +1,14 @@
+package app.evolution.pickardt.presetRules;
+
+import jasima.shopSim.core.PR;
+import jasima.shopSim.core.PrioRuleTarget;
+
+public class PRCR extends PR {
+
+	@Override
+	public double calcPrio(PrioRuleTarget entry) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
