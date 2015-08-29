3c6aa36bc5c37628aa53f9cfdf0e343de4c92fff
parkjohn <parkjohn@barretts.ecs.vuw.ac.nz>
2015-08-29 13:46:24 +1200

    Worked on the fitness classes. Now I need to write up the parameter file.

--- JSS_Research/src/app/evolution/multilevel/fitness/MultilevelGroupTWTFitness.java
+++ JSS_Research/src/app/evolution/multilevel/fitness/MultilevelGroupTWTFitness.java
@@ -1,5 +1,7 @@
 package app.evolution.multilevel.fitness;
 
+import jasima.core.statistics.SummaryStat;
+
 import java.util.Map;
 
 import app.evolution.IJasimaTracker;
@@ -7,6 +9,7 @@ import app.evolution.multilevel.IJasimaMultilevelGroupFitness;
 import app.evolution.multilevel.MLSSubpopulation;
 import ec.EvolutionState;
 import ec.Individual;
+import ec.gp.koza.KozaFitness;
 
 /**
  * TODO javadoc.
@@ -16,13 +19,21 @@ import ec.Individual;
  */
 public class MultilevelGroupTWTFitness implements IJasimaMultilevelGroupFitness {
 
+	private static final String WEIGHTED_TARDINESS = "weightedTardMean";
+
+	private SummaryStat ensembleStat = new SummaryStat();
+
 	@Override
 	public void accumulateFitness(int expIndex,
 			MLSSubpopulation subpop,
 			Map<String, Object> results,
 			IJasimaTracker tracker) {
-		// TODO Auto-generated method stub
+		// Results of a simulation over a problem instance.
+		SummaryStat stat = (SummaryStat) results.get(WEIGHTED_TARDINESS);
 
+		// We want the total weighted tardiness, so take the
+		// sum of the values accumulated by the stats object.
+		ensembleStat.value(stat.sum());
 	}
 
 	@Override
@@ -30,20 +41,40 @@ public class MultilevelGroupTWTFitness implements IJasimaMultilevelGroupFitness 
 			MLSSubpopulation subpop,
 			boolean[] updateFitness,
 			boolean shouldSetContext) {
-		// TODO Auto-generated method stub
+		int groupSize = subpop.individuals.length;
 
+		// In the MLSEvaluator, the individuals should have
+		// been evaluated, so I can safely get their fitnesses
+		// to calculate the fitness of the subpopulation.
+		double avgIndFitnesses = 0.0;
+
+		for (int i = 0; i < groupSize; i++) {
+			assert subpop.individuals[i].evaluated;
+
+			KozaFitness fitness = (KozaFitness) subpop.individuals[i].fitness;
+			avgIndFitnesses += fitness.standardizedFitness();
+		}
+
+		avgIndFitnesses /= groupSize;
+
+		// The final fitness is the linear combination of the
+		// average of the individuals fitnesses and the ensemble's
+		// fitness, multiplied by a size penalty factor.
+		double groupFitness;
+		groupFitness = (0.5 * avgIndFitnesses + 0.5 * ensembleStat.mean());
+		groupFitness *= Math.sqrt((2.0 + groupSize) / (2.0 * groupSize));
+
+		((KozaFitness) subpop.getFitness()).setStandardizedFitness(state, groupFitness);
 	}
 
 	@Override
 	public void setFitness(EvolutionState state, Individual ind) {
-		// TODO Auto-generated method stub
-
+		// FIXME Does nothing at the moment.
 	}
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-
+		ensembleStat.clear();
 	}
 
 }
--- JSS_Research/src/app/evolution/multilevel/fitness/MultilevelIndividualTWTFitness.java
+++ JSS_Research/src/app/evolution/multilevel/fitness/MultilevelIndividualTWTFitness.java
@@ -1,10 +1,13 @@
 package app.evolution.multilevel.fitness;
 
+import jasima.core.statistics.SummaryStat;
+
 import java.util.Map;
 
+import app.evolution.multilevel.IJasimaMultilevelIndividualFitness;
 import ec.EvolutionState;
 import ec.Individual;
-import app.evolution.multilevel.IJasimaMultilevelIndividualFitness;
+import ec.gp.koza.KozaFitness;
 
 /**
  * TODO javadoc.
@@ -14,22 +17,30 @@ import app.evolution.multilevel.IJasimaMultilevelIndividualFitness;
  */
 public class MultilevelIndividualTWTFitness implements IJasimaMultilevelIndividualFitness {
 
+	private static final String WEIGHTED_TARDINESS = "weightedTardMean";
+
+	private SummaryStat overallStat = new SummaryStat();
+
 	@Override
 	public void accumulateFitness(int expIndex, Map<String, Object> results) {
-		// TODO Auto-generated method stub
+		// Results of a simulation over a problem instance.
+		SummaryStat stat = (SummaryStat) results.get(WEIGHTED_TARDINESS);
 
+		// We want the total weighted tardiness, so take the
+		// sum of the values accumulated by the stats object.
+		overallStat.value(stat.sum());
 	}
 
 	@Override
 	public void setFitness(EvolutionState state, Individual ind) {
-		// TODO Auto-generated method stub
-
+		// Set the fitness is the mean total weighted
+		// tardiness over the problem instances.
+		((KozaFitness) ind.fitness).setStandardizedFitness(state, overallStat.mean());
 	}
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-
+		overallStat.clear();
 	}
 
 }
