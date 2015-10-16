41c021688621fbc805be810ee57e2730a2742558
johnskpark <johnskpark@gmail.com>
2015-10-16 18:53:17 +1300

    last commit before heading off.

--- JSS_Research/src/app/evolution/AbsGPPriorityRule.java
+++ JSS_Research/src/app/evolution/AbsGPPriorityRule.java
@@ -2,6 +2,7 @@ package app.evolution;
 
 import jasima.shopSim.core.PR;
 
+// TODO I need to get refactor this at some point.
 public abstract class AbsGPPriorityRule extends PR implements IJasimaGPPriorityRule {
 
 	private static final long serialVersionUID = 5132364772745774943L;
