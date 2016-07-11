305b321187ef8b0117607b8e1fda03c5c9a3d23f
johnskpark <johnskpark@gmail.com>
2016-07-11 19:27:42 +1200

    Machine breakdown bug fixing.

--- JSS_Research/src/app/simConfig/holthausConfig/HolthausSimConfigGenerator.java
+++ JSS_Research/src/app/simConfig/holthausConfig/HolthausSimConfigGenerator.java
@@ -15,8 +15,10 @@ public class HolthausSimConfigGenerator {
 
 	private static final double[] NUM_DUE_DATE_FACTORS = new double[]{4, 8};
 
-	private static final String REGEX = "\\(([^)]+)\\)[,]?";
 	private static final String V_CATEGORY_ALL = "all";
+
+	private static final String REGEX = "\\(([^)]+)\\)[,]?";
+	private static final double EPSILON = 0.001;
 
 	private HolthausSimConfigGenerator() {
 	}
@@ -55,10 +57,10 @@ public class HolthausSimConfigGenerator {
 
 				boolean varFound = false;
 				for (int i = 0; i < validVars.length && !varFound; i++) {
-					if (num == validVars[i]) {
-						varsList.add(num);
+					if (num > validVars[i] - EPSILON && num < validVars[i] + EPSILON) {
+						varsList.add(validVars[i]);
+						varFound = true;
 					}
-					varFound = true;
 				}
 
 				if (!varFound) {
