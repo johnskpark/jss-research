b6bf5d275e7f7a3cecca50692ccbd011cc58702e
johnskpark <johnskpark@gmail.com>
2016-07-14 20:32:45 +1200

    Machine breakdown bug fixing.

--- JSS_Research/src/app/simConfig/ExperimentGenerator.java
+++ JSS_Research/src/app/simConfig/ExperimentGenerator.java
@@ -49,9 +49,6 @@ public class ExperimentGenerator {
 
 		experiment.setStopAfterNumJobs(simConfig.getStopAfterNumJobs());
 
-		// TODO temporary code.
-		System.out.println("Repair time factor: " + simConfig.getRepairTimeFactor(index) + ", breakdown level: " + simConfig.getBreakdownLevel(index));
-
 		experiment.setRepairTimeFactor(simConfig.getRepairTimeFactor(index));
 		experiment.setBreakdownLevel(simConfig.getBreakdownLevel(index));
 		experiment.setMachineRandom(new Random(simConfig.getMachineSeed()));
