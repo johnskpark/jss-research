b4e0d6a3cd196f03670b232b26e9f55f6aa49e70
johnskpark <johnskpark@gmail.com>
2017-03-22 16:39:08 +1300

    Running the absolute value version.

--- JSS_Research/src/ec/app/GPjsp/DMOCCNSGA_MB.java
+++ JSS_Research/src/ec/app/GPjsp/DMOCCNSGA_MB.java
@@ -6,15 +6,6 @@
 
 
 package ec.app.GPjsp;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.List;
-
 import SmallStatistics.SmallStatistics;
 import ec.EvolutionState;
 import ec.Individual;
