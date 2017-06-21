29285755b87180a99a023da45d2ae4bcbdbd40e0
johnskpark <johnskpark@gmail.com>
2017-06-21 23:12:09 +1200

    Adding more broke ass code.

---
+++ JSS_Research/src/app/evolution/coop/statistics/BuggedJasimaCoopStatistics.java
@@ -0,0 +1,39 @@
+package app.evolution.coop.statistics;
+
+import app.evolution.coop.JasimaCoopProblem;
+import ec.EvolutionState;
+import ec.simple.SimpleStatistics;
+import ec.util.Parameter;
+import jasima.core.statistics.SummaryStat;
+
+public class BuggedJasimaCoopStatistics extends SimpleStatistics {
+
+	private static final long serialVersionUID = 8464431576658975312L;
+
+	@Override
+	public void setup(final EvolutionState state, final Parameter base) {
+		super.setup(state, base);
+	}
+
+	public void postEvaluationStatistics(final EvolutionState state) {
+		super.postEvaluationStatistics(state);
+
+		// The additional statistics required are:
+		// - The individual's fitness when applied to the training instances
+		// as a single dispatching rule.
+		JasimaCoopProblem problem = (JasimaCoopProblem) state.evaluator.p_problem;
+
+		if (problem.indRuleInitialised()) {
+			SummaryStat stat = problem.getAllIndStats();
+			state.output.message("Best Individual Statistics: " + stat.min());
+			state.output.println("Best Individual Statistics: " + stat.min(), statisticslog);
+
+			SummaryStat[] statPerSubpop = problem.getIndStatPerSubpop();
+			for (int i = 0; i < statPerSubpop.length; i++) {
+				state.output.message("Best Individual Statistics Subpop " + i + ": " + statPerSubpop[i].min());
+				state.output.println("Best Individual Statistics Subpop " + i + ": " + statPerSubpop[i].min(), statisticslog);
+			}
+		}
+	}
+
+}
