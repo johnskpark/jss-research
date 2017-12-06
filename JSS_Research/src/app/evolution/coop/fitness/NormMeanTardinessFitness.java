ac874d07e1a2db84c0a907dc885656e5145f1147
johnskpark <johnskpark@gmail.com>
2017-12-07 10:09:56 +1300

    Committing the last changes made before I start implementing this niching approach.

---
+++ JSS_Research/src/app/evolution/coop/fitness/NormMeanTardinessFitness.java
@@ -0,0 +1,30 @@
+package app.evolution.coop.fitness;
+
+import java.util.List;
+import java.util.Map;
+
+import app.evolution.IJasimaFitness;
+import app.evolution.JasimaFitnessBase;
+import app.evolution.coop.JasimaCoopFitness;
+import app.evolution.coop.JasimaCoopIndividual;
+import app.stat.TardinessStat;
+import ec.Individual;
+
+public class NormMeanTardinessFitness extends JasimaCoopFitness {
+
+	@Override
+	protected IJasimaFitness<JasimaCoopIndividual> generateFitness(final Individual[] inds) {
+		return new IndividualTWTFitness();
+	}
+
+	private class IndividualTWTFitness extends JasimaFitnessBase<JasimaCoopIndividual> {
+
+		@Override
+		public double getFitness(int expIndex, JasimaCoopIndividual ind, Map<String, Object> results) {
+			List<Double> referenceStat = getProblem().getReferenceStat();
+
+			return TardinessStat.getNormMeanTardiness(results, referenceStat.get(expIndex));
+		}
+	}
+
+}
